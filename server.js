8a299f3757c27c759d2ff90e8451a3abe672dda1
yamapi06150 <yamapi06150@gmail.com>
2016-09-29 15:52:06 +0800

    third commit - add some port_string and env_string control and mongodbURL of DEV

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 var express = require('express');
 var app = express();
 
+var port_value = 4000;
+var env_string = process.argv[2];
+var port_string = process.argv[3];
+var mongodbURL ;
+
+
+if( typeof port_string !== 'undefined' && port_string.length != 0 ){
+    port_value = port_string ;
+}
+
+if( typeof env_string === 'undefined'){
+    console.log('env_string is not set！');
+    return -1;
+}else{
+    switch(env_string){
+        case 'DEV':
+            //dev db setting
+            mongodbURL = 'mongodb://localhost:27017/Noddy2';
+            break;
+        default:
+            console.log('env_string is not valid！');
+            return -1;
+    }
+
+}
 
 app.get('/hello',function(req,res){
     res.type('text/plain');
     res.send('aloha~');
 });
 
-app.listen(4000);
+app.listen(port_value);
 
 console.log(process.argv.length);
 console.log('prgram_name = process.argv[0]：' + process.argv[0]);
