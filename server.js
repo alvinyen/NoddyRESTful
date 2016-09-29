05e151ab385b07c0c48e900be9e7a8c9a9f8f25b
yamapi06150 <yamapi06150@gmail.com>
2016-09-29 18:25:51 +0800

    sixth commit - add static file including 404.html n 500.html

--- server.js
+++ server.js
@@ -29,6 +29,18 @@ if( typeof env_string === 'undefined'){
 
 app.set( 'public' , path.join( __dirname , 'public'));
 app.use(express.static(app.get('public'))); //在靜態資料夾裡會自動找到index.html
+app.use(function ( req , res , next ){
+    res.status(404);
+    res.sendFile(path.join( app.get('public') , '404.html'));
+});
+app.use(function ( err , req , res , next){
+    console.log(err.stack);
+
+    res.status(500);
+    res.sendfile(app.get('public') , '500.html');
+});
+
+
 
 app.get('/hello',function(req,res){
     res.type('text/plain');
