18d928b0425cac48688fb83046ff5e5cc39cb2d6
yamapi06150 <yamapi06150@gmail.com>
2016-09-29 19:23:24 +0800

    seventh commit - adding db connection and assertion setting

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert'); // for assert db connection
 var path = require('path');
 var express = require('express');
 var app = express();
@@ -5,7 +7,9 @@ var app = express();
 var port_value = 4000;
 var env_string = process.argv[2];
 var port_string = process.argv[3];
+
 var mongodbURL ;
+var db ;
 
 if( typeof port_string !== 'undefined' && port_string.length != 0 ){
     port_value = port_string ;
@@ -18,7 +22,7 @@ if( typeof env_string === 'undefined'){
     switch(env_string){
         case 'DEV':
             //dev db setting
-            mongodbURL = 'mongodb://localhost:27017/Noddy2';
+            mongodbURL = 'mongodb://localhost:27017/Noddy';
             break;
         default:
             console.log('env_string is not valid！');
@@ -27,6 +31,8 @@ if( typeof env_string === 'undefined'){
 
 }
 
+
+//routes
 app.set( 'public' , path.join( __dirname , 'public'));
 app.use(express.static(app.get('public'))); //在靜態資料夾裡會自動找到index.html
 app.use(function ( req , res , next ){
@@ -40,15 +46,22 @@ app.use(function ( err , req , res , next){
     res.sendfile(app.get('public') , '500.html');
 });
 
-
-
 app.get('/hello',function(req,res){
     res.type('text/plain');
     res.send('aloha~');
 });
 
+//mongodb
+MongoClient.connect( mongodbURL , function ( err , dbConnection ){
+    assert.equal(null,err);
+    console.log( ' connected successfully to mongodb server ' + mongodbURL);
 
-app.listen(port_value);
+    db = dbConnection ; //全域
+    app.set( 'dbConnection' , dbConnection );
+
+    app.listen(Number(port_value));
+    console.log('server is running on ' + Number(port_value));
+});
 
 console.log(process.argv.length);
 console.log('prgram_name = process.argv[0]：' + process.argv[0]);
