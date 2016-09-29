7702004260ed2a4ad1ecfed36efb00c2620d485b
yamapi06150 <yamapi06150@gmail.com>
2016-09-29 15:35:17 +0800

    second commit - add some env_variables

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ app.get('/hello',function(req,res){
 });
 
 app.listen(4000);
+
+console.log(process.argv.length);
+console.log('prgram_name = process.argv[0]：' + process.argv[0]);
+console.log('script_path/entry file = process.argv[1]：' + process.argv[1]);
+console.log('_dirname：' + __dirname); //must type two continuous underline...
+console.log('_filename：'+ __filename); //must type two continuous underline...
+
